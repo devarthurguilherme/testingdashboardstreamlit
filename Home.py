5698b1ce66d8f9e35910b9dc73073c337f432340
Arthur Guilherme <104983447+devarthurguilherme@users.noreply.github.com>
2024-07-24 15:44:00 +0100

    u

--- Home.py
+++ Home.py
@@ -1,8 +1,23 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import plotly.graph_objects as go
 # from dataset import df
 # from graphics import ageGroupCounts
+
+
+#################
+
+df = pd.DataFrame(
+    [
+        {"command": "st.selectbox", "rating": 4, "is_widget": True},
+        {"command": "st.balloons", "rating": 5, "is_widget": False},
+        {"command": "st.time_input", "rating": 3, "is_widget": True},
+    ]
+)
+
+
+#########
 
 
 # Layout Wide Origin Config
@@ -19,9 +34,7 @@ tab1, tab2, tab3 = st.tabs(
 with tab1:
     st.subheader("Dataset Drivers")
     st.write('This Dataframa is about ...')
-    chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
-
-    st.bar_chart(chart_data)
+    st.dataframe(df, use_container_width=True)
     # st.dataframe(newDfDrivers)
     # Show Full Dataframe
     # st.dataframe(df)
@@ -29,6 +42,8 @@ with tab1:
 # Tab 2
 with tab2:
     st.subheader("First analisys about ...")
+    chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+    st.bar_chart(chart_data)
     # chart_data = ageGroupCounts
     # st.bar_chart(chart_data)
 
@@ -38,4 +53,19 @@ with tab2:
 
 # Tab 3
 with tab3:
-    st.subheader("Second analisys about ...")
+    # Dados de exemplo
+    categorias = ['A', 'B', 'C']
+    valores = [10, 20, 15]
+
+    # Criar o gráfico de barras
+    fig = go.Figure(data=[go.Bar(x=categorias, y=valores)])
+
+    # Personalizar o layout do gráfico
+    fig.update_layout(
+        title='Gráfico de Barras com Plotly',
+        xaxis_title='Categorias',
+        yaxis_title='Valores'
+    )
+
+    # Mostrar o gráfico no Streamlit
+    st.plotly_chart(fig)
